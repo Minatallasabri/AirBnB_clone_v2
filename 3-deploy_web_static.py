ea9b4d8355223b784754e3cc5141761d80cd6ba1
Minatallasabri <minatallasabri3380@gmail.com>
2024-05-19 16:33:54 +0300

    Try fixing Task 2 , 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
-script based on the file 2-do_deploy_web_static.py that creates and
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 
 execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
@@ -10,7 +9,7 @@ execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['54.160.77.90', '10.25.190.21']
+env.hosts = ["54.175.222.189", "54.157.184.160"]
 
 
 def do_pack():
@@ -19,11 +18,11 @@ def do_pack():
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-            file_name = "versions/web_static_{}.tgz".format(date)
-            local("tar -cvzf {} web_static".format(file_name))
-            return file_name
-        except:
-            return None
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
@@ -37,67 +36,19 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))                                                                                                                      run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-=======
-"""Create and distributes an archive to web servers"""
-import os.path
-import time
-from fabric.api import local
-from fabric.operations import env, put, run
-
-env.hosts = ['54.175.18.167', '35.174.207.14']
-
-
-def do_pack():
-    """Generate an tgz archive from web_static folder"""
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
-    except:
-        return None
-
-
-def do_deploy(archive_path):
-    """Distribute an archive to web servers"""
-    if (os.path.isfile(archive_path) is False):
-        return False
-
-    try:
-        file = archive_path.split("/")[-1]
-        folder = ("/data/web_static/releases/" + file.split(".")[0])
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(folder))
-        run("tar -xzf /tmp/{} -C {}".format(file, folder))
-        run("rm /tmp/{}".format(file))
-        run("mv {}/web_static/* {}/".format(folder, folder))
-        run("rm -rf {}/web_static".format(folder))
-        run('rm -rf /data/web_static/current')
-        run("ln -s {} /data/web_static/current".format(folder))
-        print("Deployment done")
->>>>>>> b900490075149fa5f8aa4377f17e5fb2ccf6241d
         return True
     except:
         return False
 
 
 def deploy():
-<<<<<<< HEAD
     """creates and distributes an archive to the web servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
     return do_deploy(archive_path)
-=======
-    """Create and distributes an archive to web servers"""
-    try:
-        path = do_pack()
-        return do_deploy(path)
-    except:
-        return False
->>>>>>> b900490075149fa5f8aa4377f17e5fb2ccf6241d
