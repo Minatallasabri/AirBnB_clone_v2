fcc540d2c416c452ea3ae6a56a131dea20604859
Minatallasabri <minatallasabri3380@gmail.com>
2024-05-20 17:03:13 +0300

    Mina's fix task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,55 +1,42 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers
-"""
-
+""" Fabric script that generates a .tgz archive """
+from fabric.decorators import task
+from fabric.api import *
 from datetime import datetime
-from fabric.api import put, run
-from fabric.api import env
-from fabric.api import local
 import os
 
 env.hosts = ["54.175.222.189", "54.157.184.160"]
-env.user = "ubuntu"
-env.key_filename = '~/.ssh/id_rsa'
 
 
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
-
-
+@task
 def do_deploy(archive_path):
-    """
-        Distribute archive.
-    """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
+    """Fabric script that distributes an archive to web servers"""
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        with_ext = archive_path.split("/")[-1]
+        without_ext = archive_path.split("/")[-1].split(".")[0]
+        pth = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
+        run("mkdir -p " + pth + without_ext)
+        run("tar -xzf /tmp/{} -C {}{}/".format(with_ext, pth, without_ext))
+        run("rm /tmp/{}".format(with_ext))
+        run("mv {1}{0}/web_static/* {1}{0}/".format(without_ext, pth))
+        run("rm -rf {}{}/web_static".format(pth, without_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(pth, without_ext))
         return True
+    except Exception as e:
+        print("An error occurred: {}".format(e))
+        return False
 
-    return False
+
+@task
+def do_pack():
+    """generates a .tgz archive from web_static"""
+    datestr = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "versions/web_static_{}.tgz".format(datestr)
+    local("mkdir -p versions")
+    if local("tar -cvzf {} web_static/".format(file_name)).succeeded:
+        return file_name
+    return None
